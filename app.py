6bf5f25c33db9fde333f29d19d71d8b1ac89683e
nacool <nakul.kulkarni@provakil.com>
2022-08-09 22:19:13 +0530

    new frontend and new design

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ cont_count = 5
 @app.route('/',defaults={'page':1})
 @app.route('/<page>')
 def index(page):
+    current = int(page)
+    if current < 1:
+        current = 1
     sch = data
-    from_page = (int(page)-1)*cont_count
-    to_page = int(page)*cont_count
-    pagination = math.ceil(len(data)/cont_count)
-    return render_template('index.html',data=sch,from_page=from_page,to_page=to_page,pagination=pagination)
+    from_page = (current-1)*cont_count
+    to_page = current*cont_count
+    
+    return render_template('index.html',data=sch,from_page=from_page,to_page=to_page,current=current)
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=8080)
