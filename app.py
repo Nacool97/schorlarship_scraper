e713b5904449465b4331e4452e032f6ec863d328
nacool <nacoolthedj@gmail.com>
2022-08-19 17:36:53 +0530

    new filter added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 cleint = MongoClient('localhost',27017)
 collection = cleint['nacool_projects']['scholarships']
-data = list(collection.find())
+data = list(collection.find({'expired':False}))
 page_number = 1
 cont_count = 5
 @app.route('/',defaults={'page':1})
