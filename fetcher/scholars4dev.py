46a8b7f7edc0f4ab39668a900b1a8fc2229e4989
nacool <nacoolthedj@gmail.com>
2022-08-22 20:44:45 +0530

    added number of days left check

--- fetcher/scholars4dev.py
+++ fetcher/scholars4dev.py
@@ -67,6 +67,7 @@ def parser_webpage(content):
         scholarship['schship_for'] = data_list[0].text.strip()
         deadline = data_list[1].text.strip()
         m = re.search(r'(\d+\s*[a-z]+\s*\d{4})',deadline, re.I) 
+        number_of_days = 0
         if m:
             deadline = m.group(1)
             try:
