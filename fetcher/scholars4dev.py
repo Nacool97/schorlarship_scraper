5e845ff183208bf5e0a6176f7984fd3bb993b831
nacool <nacoolthedj@gmail.com>
2022-08-23 10:14:34 +0530

    minor changes

--- fetcher/scholars4dev.py
+++ fetcher/scholars4dev.py
@@ -1,7 +1,7 @@
 import re
 import requests
 import json
-from datetime import datetime
+from datetime import datetime,timedelta
 import pika
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
@@ -67,7 +67,7 @@ def parser_webpage(content):
         scholarship['schship_for'] = data_list[0].text.strip()
         deadline = data_list[1].text.strip()
         m = re.search(r'(\d+\s*[a-z]+\s*\d{4})',deadline, re.I) 
-        number_of_days = 50
+        number_of_days = (datetime.now + timedelta(days=50)).timestamp()
         if m:
             deadline = m.group(1)
             try:
