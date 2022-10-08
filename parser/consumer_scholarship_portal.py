89727342ccc771a491240f46e1006239e527b1c3
nacool <nacoolthedj@gmail.com>
2022-10-08 15:47:50 +0530

    new consumer

--- parser/consumer_scholarship_portal.py
+++ parser/consumer_scholarship_portal.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 channel.queue_declare(queue='new_data_scholarship_portal')
 
 def callback(ch,method,properties,body):
-    print(body, type(body))
+    print(list(body)[0], type(body))
 
 channel.basic_consume(queue='new_data_scholarship_portal',auto_ack=True,on_message_callback=callback)
 channel.start_consuming()
