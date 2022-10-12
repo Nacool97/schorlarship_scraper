dd63524b4aad66b8d13392c0f904434128dce2f7
nacool <nacoolthedj@gmail.com>
2022-10-12 22:17:30 +0530

    minor changes

--- parser/consumer_scholarship_portal.py
+++ parser/consumer_scholarship_portal.py
@@ -8,7 +8,7 @@ channel = connection.channel()
 channel.queue_declare(queue='new_data_scholarship_portal')
 
 def callback(ch,method,properties,body):
-    print(json.loads(body)[0], type(body))
+    print(json.loads(body), type(body))
 
 channel.basic_consume(queue='new_data_scholarship_portal',auto_ack=True,on_message_callback=callback)
 channel.start_consuming()
