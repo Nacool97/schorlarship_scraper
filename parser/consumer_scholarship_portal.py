103ed90fd2fac2bd0a320b77a01af5143514d9c1
nacool <nacoolthedj@gmail.com>
2022-10-12 19:08:30 +0530

    new changes

--- parser/consumer_scholarship_portal.py
+++ parser/consumer_scholarship_portal.py
@@ -1,5 +1,6 @@
 import pika
-import parser_scholars4dev
+import json
+#import parser_scholars4dev
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
@@ -7,7 +8,7 @@ channel = connection.channel()
 channel.queue_declare(queue='new_data_scholarship_portal')
 
 def callback(ch,method,properties,body):
-    print(list(body)[0], type(body))
+    print(json.loads(body)[0], type(body))
 
-channel.basic_consume(queue='new_data_scholarship_portal',auto_ack=False,on_message_callback=callback)
+channel.basic_consume(queue='new_data_scholarship_portal',auto_ack=True,on_message_callback=callback)
 channel.start_consuming()
