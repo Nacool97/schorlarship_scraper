c53569e311b63df7722c5c142c71578b69796ba9
nacool <nakul.kulkarni@provakil.com>
2023-01-08 13:38:26 +0530

    new mailer feature

--- parser/notifier/mail_notifier.py
+++ parser/notifier/mail_notifier.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mail import Mail, Message
 import mysql.connector
 from pymongo import MongoClient
+from bson import ObjectId
 from datetime import datetime
 app = Flask(__name__)
 app.secret_key = "any@random#string"
@@ -20,6 +21,8 @@ mail = Mail(app)
 # get the mongodb access
 cleint = MongoClient('localhost', 27017)
 collection = cleint['nacool_projects']['scholarships']
+
+
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
@@ -43,9 +46,51 @@ def get_all_email_id(results):
     for result in results:
         alerts =()
         try:
-            deadline = datetime.strptime(result[1],"%d/%m/%YYYY")
+            deadline = datetime.strptime(result[2],"%d/%m/%Y")
         except Exception as e:
             deadline = None
-        if deadline and deadline - today:
-            pass
+        if deadline and (deadline - today).days <= 7:
+            alerts = (result[0], result[1])
+        if alerts:
+            alerts_list.append(alerts)
+    return alerts_list
+
+def update_alert_flag(email,scholarship_id):
+    query = f"update scholars_subs set send_alert = {False} where scholarship_id = '{scholarship_id}' and email = '{email}'"
+    cursor.execute(query)
+    mydb.commit()
+    print(cursor.rowcount, "row(s) updated")
+
+# send alerts and update the send_alerts flag
+def send_mail_and_update(data):
+    all_scholarships = list(collection.find({"expired":{"$ne":True}}))
+    for d in data:
+        scholarship_id = d[1]
+        email = d[0]
+        for sch in all_scholarships:
+            if str(sch.get('_id')) == scholarship_id:
+                send_mail(email, sch)
+                update_alert_flag(email,scholarship_id)
+    return "Done"
+
+def send_mail(email, scholarship):
+    name = "User"
+    message = Message(
+        f"Scholarship Deadline Alert ",
+        sender='nacool.scholarsmate@gmail.com',
+        recipients=[email]
+    )
+    message.html = f'Hello {name},<br>This the deadline alert for the {scholarship["name"]}.<br>Deadline is on {scholarship["deadline"]}.<br>Here is the <a href="https://scholarslsmate.uk/view/{scholarship["_id"]}">scholarship url</a>'
+    mail.send(message)
+    return 'Sent'
+@app.route("/")
+def send_notification():
+    data = get_all_mails_for_alerts()
+    print(data)
+    result = get_all_email_id(data)
+    print(result)
+    return send_mail_and_update(result)
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",port=5000)
 
